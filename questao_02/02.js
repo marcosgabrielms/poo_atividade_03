0f596b4479fc238cf04a88afb955c795e946c275
Marcos Gabriel <marcosgabriel166@gmail.com>
2025-09-27 15:03:24 -0300

    :sparkles: feat: adiciona questão 02

---
+++ questao_02/02.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function saudar(nome, pronome = "Sr") {
+    console.log(`${pronome}. ${nome}`);
+}
+saudar("Carlos");
+saudar("Augusto", "Dr");
+saudar("Presidente Ely", "Exmo");
+saudar("Sávia", "Sra");
+//# sourceMappingURL=02.js.map
---
+++ questao_02/02.ts
@@ -0,0 +1,8 @@
+function saudar(nome: string, pronome: string = "Sr"): void { //Função Void executa apeans uma aç
+    console.log(`${pronome}. ${nome}`);
+}
+
+saudar("Carlos");
+saudar("Augusto", "Dr");
+saudar("Presidente Ely", "Exmo");
+saudar("Sávia", "Sra");
