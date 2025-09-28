67ca1bf72eb72a8c86954389131f7e48530d5c65
Marcos Gabriel <marcosgabriel166@gmail.com>
2025-09-28 00:34:37 -0300

    :sparkles: feat: adiciona questão 13

---
+++ questao_13/13.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class TradutorEmojis {
+    dicionario = {
+        "amor": "❤️",
+        "futebol": "⚽",
+        "cachorro": "🐶",
+        "casa": "🏠",
+        "sol": "☀️",
+        "dinheiro": "💵",
+        "brasil": "🇧🇷"
+    };
+    traduzir(frase) {
+        const palavras = frase.split(' ');
+        const palavrasTraduzidas = palavras.map(palavra => {
+            const palavraChave = palavra.toLowerCase();
+            return this.dicionario[palavraChave] || palavra;
+        });
+        return palavrasTraduzidas.join(' ');
+    }
+}
+const tradutor = new TradutorEmojis();
+const fraseOriginal = "O amor do brasileiro é o futebol e ter um cachorro em casa";
+console.log(`Frase Original: ${fraseOriginal}`);
+const fraseTraduzida = tradutor.traduzir(fraseOriginal);
+console.log(`Frase Traduzida: ${fraseTraduzida}`);
+const outraFrase = "No Brasil, o Sol brilha e o Cachorro late.";
+console.log(`\nFrase Original: ${outraFrase}`);
+console.log(`Frase Traduzida: ${tradutor.traduzir(outraFrase)}`);
+//# sourceMappingURL=13.js.map
