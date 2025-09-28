4decfb1e5e60620115b0c74dd3025907f2f37d69
Marcos Gabriel <marcosgabriel166@gmail.com>
2025-09-28 00:33:27 -0300

    :sparkles: feat: adiciona questão 04

---
+++ questao_04/04.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function soma(x, y) {
+    return x + y;
+}
+console.log(soma(1, 2));
+console.log(soma(1, "2"));
+console.log(soma(1));
+//# sourceMappingURL=04.js.map
